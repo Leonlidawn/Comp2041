6aa8e61f4116f6bfba7724aed880fb48ae6b64be
Weidong Li <wlix528@cse.unsw.EDU.AU>
2014-10-09 23:31:37 +1100

    final

--- assn1/python2perl.pl
+++ assn1/python2perl.pl
@@ -5,13 +5,6 @@
 #student id :z3447627
 
 
-#corrected matching regex for variable 
-
-
-#a array which stores all variables.
-#@variables=();
-#@operatoers=("+", "-", "*", "/", "%", "**");
-
 while ($line = <>) {
 	if ($line =~ /^#!/ && $. == 1) {
 		# translate #! line 
@@ -19,31 +12,25 @@ while ($line = <>) {
 	} 
 	elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/ ) {
 		# Blank & comment lines can be passed unchanged
-		#print"---------------------enter 1\n";
 		print $line;
 	
 	} 
-	elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {#contains variable in ""
-			#print"---------------------enter 2\n";
-			print &perlPrintf ($line)."\n";
-	}
-	elsif ($line =~ /^\s*print\s*[^""]*r".*/){#printing raw string
-			#print"---------------------enter 3\n";
+	elsif ($line =~ /^\s*print\s*[^""]*r".*/){
+			#printing raw strings
 			print &perlRawPrint($line)."\n";		
 	}
-	elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {#contains variable in ""
-			#print"---------------------enter 4\n";
+	elsif ($line =~ /^\s*print\s*"(.*)"\s*%/) {
+			#this line is in printf-like format 
 			print &perlPrintf($line)."\n";
 	}
-	elsif($line =~ /^\s*print\s/){#contains variable outside
-			#print"---------------------enter 5\n";
+	
+	elsif($line =~ /^\s*print\s/){
+			#contains variable outside"" or no variables inside""
 			print &perlVarPrint($line)."\n";
 	}
-	elsif($line =~ /[a-zA-Z0-9_]+.*=.* /){#normal assigning
-			#print"---------------------enter 6\n";
-			$line =~ s/([a-zA-Z0-9_]*[a-zA-Z]+[a-zA-Z0-9_]*)/\$$1/g ;
-			chomp $line;
-			print $line.';'."\n";
+	elsif($line =~ /[a-zA-Z0-9_]+.*=.* /){
+			#normal statement assigning values to variables
+			print &perlAssn($line)."\n";
 	}
 	 else {
 	
@@ -52,12 +39,48 @@ while ($line = <>) {
 		print "#$line\n";
 	}
 }
-sub perlVarPrint{
+
+
+
+sub perlAssn{
+	#divide the line into 2 
+	#q is the content in "" and l is the rest
 	my $l = $_[0];
-	$l =~ s/^\s*print//;
-	$l = perlVar($l,1);
-	$l =~ s/,/."\\s",/g;
-	$perlLine = "print ".$l.';';
+	my $q = "";
+	$l =~ s/\"\s*\+\s*\"/\"\.\"/g;
+	if($l =~ m/(\".*\")/){
+	  $q = $1;
+	  $l =~ s/$q//;
+	}
+
+	$l =~ s/([a-zA-Z0-9_]*[a-zA-Z]+[a-zA-Z0-9_]*)/\$$1/g ;
+	chomp $l;
+	
+ 	my $perlLine = $l.$q.';';
+	return $perlLine;
+}
+sub perlVarPrint{
+	#divide the line into 2 
+	#q is the content in "" and l is the rest
+	my $l = $_[0];
+	$l =~ s/\"\s*\+\s*\"/\"\.\"/g;
+	chomp $l;
+	my $q = $l;
+	$q =~ s/^\s*print\s*//g;
+	chomp $q;
+	if (!($q =~ m/^(".*").*/)){
+		$q="";
+	}
+	$l =~ s/^\s*print\s*//;
+	$l =~ s/^\s".*"\s*//;
+	chomp $l;
+	if ($l){
+		$q = $q.',' if($q);
+		$l = perlVar($l) ; 
+		$l =~ s/,/."\\s",/g;
+	}
+	my $perlLine = "print ".$q.$l.';';
+	
 	return $perlLine;
 }
 
@@ -68,41 +91,43 @@ sub perlRawPrint{
 	return $l.'."\n";';
 }
 
-sub perlPrintf { #need to consider variables
+sub perlPrintf {
+	#divide the line into 2 
+	#inq keeps the content in quotes and var stores the variables
 	my $l = $_[0]; #print"%s %s %d" % (v1,v2,v3)
-	print $l."\n";
+	#print $l."\n";
+		chomp $l;
 		my $var = $l;
 		my $inq = "";
-		$var =~ s/print\".*\"//; # % (v1,v2,v3)
+		$var =~ s/print.*\".*\"//; # % (v1,v2,v3)
 		$var =~ s/\%//; # (v1,v2,v3)
 		$var =~ s/[\(\)]//g; # v1,v2,v3
-		$var = &perlVar($var,1);
+		$var = &perlVar($var);
+		
 		($inq) = ($l =~ /(\".*\")/);
 		if(($inq) && ($var)){
 			$inq = $inq.',';	
 		}
 		$perlLine = "printf".'('.$inq.$var.'."\n");';
-	
+		
 	return $perlLine; 
 }
 
 sub perlVar{#deduce if there are variables and convert them to perl format if there is any.
-	(my $l,$con) = @_;
-	if($con){ 
-		my @var = split (/,/,$l); 
-		$v = shift @var;
-		$v =~ s/ //g;#remove spaces
-		$vsInPerl = "\$$v";
+	(my $l) = @_;
+	my @var = split (/,/,$l); 
+	$v = shift @var;
+	$v =~ s/ //g;#remove spaces
+	$vsInPerl = "\$$v";
 		
-		foreach $v (@var){
-			$v =~ s/ //g;#remove spaces
-			$vsInPerl .= ",\$$v";
-		}
-	
-		chomp $vsInPerl;
-		return $vsInPerl;
-
+	foreach $v (@var){
+		$v =~ s/ //g;#remove spaces
+		$vsInPerl .= ",\$$v";
 	}
+	
+	chomp $vsInPerl;
+	return $vsInPerl;
+
 
 }
 
