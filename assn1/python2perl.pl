019c051e01a35ff61a965a925905e9954a3dda38
Weidong Li <wlix528@cse.unsw.EDU.AU>
2014-10-09 23:26:48 +1100

    debugging

--- assn1/python2perl.pl
+++ assn1/python2perl.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl -w
+#Aimed to convert numeric constants, arithmetic operation and print statements only from python to perl only.
+
 #comp2041 assignment 1
 #student id :z3447627
 
-#subroutine perlPrint implemented in normal txt file.
+
+#added @operators array to enhance variables detection in perlVar subroutine.
 
 
 #a array which stores all variables.
 @variables=();
-
+@operatoers=("+", "-", "*", "/", "%", "**");
 
 while ($line = <>) {
 	if ($line =~ /^#!/ && $. == 1) {
@@ -16,26 +19,14 @@ while ($line = <>) {
 	} 
 	elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
 		# Blank & comment lines can be passed unchanged
-		print $line;
+		print $line."\n";
 	
 	} 
-	elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {#variable needed
-		if($line =~ %){
-
-		}
+	elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+		
+			perlPrint ($line);
+	}
 	# Python's print print a new-line character by default
-		# so we need to add it explicitly to the Perl print statement
-		#check if variable is used.
-
-		if (){ #if there are new var,push in.
-			push @variables, $cvar;
-		}
-
-	 elsif(){#check for new variable
-
-	 
-
-
 
 	 else {
 	
@@ -51,21 +42,42 @@ sub perlPrint { #need to consider variables
 	$l = $_; #print"%s %s %d" % (v1,v2,v3)
 	#handle the print;
 		#print"%s %s %d" % (v1,v2,v3)
-	
-		$var =~ s/print".*"//,$l; # % (v1,v2,v3)
+		$var = $l;
+		$var =~ s/print".*"//; # % (v1,v2,v3)
 		$var =~ s/%//; # (v1,v2,v3)
 		$var =~ s/)//; # v1,v2,v3
-	
-		@var =~ split (/,/,$var); 
-		foreach $v (@var){
-			$vsInPerl .= ",\$$v";
-		}
-
-		$l =~ /printf".*"/;
+		$var = perlVar($var,1);
+		$var = ",".$var;
+		$l =~ /".*"/;
 		
-		$l = join ("printf", "\(", $section, ', "\n"', "\)");
+		$perlLine = "printf"."\("."$1".$var, ', "\n"', "\)\;");
 	}
-	return $l; 
+	return $perlLine; 
+}
+
+sub perlVar{#deduce if there are variables and convert them to perl format if there is any.
+	($l,$con) = @_;#if $con is true, it skips checking and consider $l contains variables only.
+	if($con){ 
+		@var =~ split (/,/,$l); 
+		foreach $v (@var){
+			$v =~ s/ //g;#remove spaces
+			$vsInPerl .= ",\$$v";
+		}	
+		return $vsInPerl;
+	}
+	else{#not in (), need to check for  
+		@var =~ split (/ /,$l); 
+		foreach $v (@var){
+			if ($v =~ /[a-zA-Z0-9]+/){
+	
+			}
+			$v =~ s/ //g;#remove spaces
+			$vsInPerl .= ",\$$v";
+		}	
+		return $vsInPerl
+
+	}
+
 }
 
 sub perlFormat(){
